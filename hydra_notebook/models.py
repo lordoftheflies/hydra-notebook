3eeb038d21e42ea7203199c1cc70239548e12b63
László Hegedűs <heglas11@gmail.com>
2018-09-06 00:50:14 +0200

    Stage.

--- hydra_notebook/models.py
+++ hydra_notebook/models.py
@@ -1,8 +1,8 @@
 from django.db import models
 
 # Create your models here.
-class ManagedNotebookModel(models.Model):
-    name = models.CharField(max_length=100)
-    path = models.FilePathField(allow_files=True, allow_folders=False)
-
-
+# class ManagedNotebookModel(models.Model):
+#     name = models.CharField(max_length=100)
+#     path = models.FilePathField(allow_files=True, allow_folders=False)
+#
+#
